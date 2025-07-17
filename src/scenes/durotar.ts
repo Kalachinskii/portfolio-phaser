46a36419ea3c7d4f047dbccac1a6dc55945b8ad1
Vladislav <kalachinskii_vl@mail.ru>
2025-07-17 20:55:40 +0300

    добавлен персонаж

--- src/scenes/durotar.ts
+++ src/scenes/durotar.ts
@@ -1,16 +1,29 @@
 import testJSON from "../assets/test.json";
-import { LAYERS, SIZES, TITLES } from "../utils/constants";
+import { Player } from "../entities/player";
+import { LAYERS, SIZES, SPRITES, TITLES } from "../utils/constants";
 
 export class Durotar extends Phaser.Scene {
+  private player?: Player;
+
   constructor() {
     super("DurotarScene");
   }
 
+  // предзагрузка
   preload() {
     this.load.image(TITLES.NAME_MAP, "src/assets/b.png");
     this.load.tilemapTiledJSON("map", "src/assets/test.json");
+    this.load.spritesheet(
+      SPRITES.PLAYER,
+      "src/assets/characters/alliance.png",
+      {
+        frameWidth: SIZES.PLAYER.WIDTH,
+        frameHeight: SIZES.PLAYER.HEIGHT,
+      }
+    );
   }
 
+  // создание определенных моментов
   create() {
     const map = this.make.tilemap({ key: "map" });
     // addTilesetImage(name из json файла talses->name)
@@ -29,5 +42,10 @@ export class Durotar extends Phaser.Scene {
     // слои 1 и 2
     const groundLayer = map.createLayer(LAYERS.GROUND, tileset, 0, 0);
     const wallsLayer = map.createLayer(LAYERS.WALLS, tileset, 0, 0);
+    // класс сцены, кардинаты, текстурный ключ из прелоад
+    this.player = new Player(this, 400, 250, SPRITES.PLAYER);
   }
+
+  // реализация анимаций, действий на клавиши
+  update(time: number, delta: number): void {}
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,10 +5,18 @@ export const TITLES = {
 
 export const SIZES = {
   TILE: 32,
+  PLAYER: {
+    WIDTH: 48,
+    HEIGHT: 48,
+  },
 };
 
 // слои
 export const LAYERS = {
   GROUND: "1",
   WALLS: "2",
+};
+
+export const SPRITES = {
+  PLAYER: "Player",
 };
