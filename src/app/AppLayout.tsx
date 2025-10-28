3420c7558562bf0299edcca41cd80df23ffd1575
Vladislav <kalachinskii_vl@mail.ru>
2025-10-28 16:18:21 +0300

    fix: компаненты перенесены в лаяют

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import { Headers } from "../pages/Layout/Headers";
 import { Footer } from "../pages/Layout/Footer";
+import { Controls } from "../companents/controls/Controls";
+import { Status } from "../companents/status/Status";
+import styles from "./Home.module.css";
+import heroImage from "../assets/hero.png";
 
 export function AppLayout() {
   return (
     <>
       <Headers />
-      <Outlet />
+      <div className={styles.container}>
+        <img className={styles.img} src={heroImage} alt="hero img" />
+        <Status />
+        <Outlet />
+        <Controls />
+      </div>
       <Footer />
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import { useParams } from "react-router-dom";
 import { useGame } from "../config/useGame";
-import styles from "./Home.module.css";
-import heroImage from "../assets/hero.png";
-import { Controls } from "../companents/controls/Controls";
-import { Status } from "../companents/status/Status";
 
 export function Home() {
   const { locationId } = useParams();
@@ -11,11 +7,6 @@ export function Home() {
   const { gameContainerRef } = useGame(locationId || "Home");
 
   return (
-    <div className={styles.container}>
-      <img className={styles.img} src={heroImage} alt="hero img" />
-      <Status />
-      <div ref={gameContainerRef} style={{ width: "800px", height: "600px" }} />
-      <Controls />
-    </div>
+    <div ref={gameContainerRef} style={{ width: "800px", height: "600px" }} />
   );
 }
