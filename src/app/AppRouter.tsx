3f20d621f82a51c94afe8719ec7274f184c69cdd
Vladislav <kalachinskii_vl@mail.ru>
2025-10-28 17:24:16 +0300

    fix: исправление путей

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,17 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Street } from "../pages/Street";
 import { AppLayout } from "./AppLayout";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      {
-        path: "/street",
-        element: <Street />,
-      },
+      { path: "/street", element: <Street /> },
+      { path: "/Home", element: <Home /> },
     ],
   },
 ]);
